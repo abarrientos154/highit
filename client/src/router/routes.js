266e3cae9ad3f440ec2f85246568ce38d258c813
Examplee <example@example.com>
2021-07-14 21:57:18 -0400

    arreglo layaut

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -4,12 +4,12 @@ const routes = [
     path: '/home',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '/inicio', component: () => import('pages/Home.vue') }
+      { path: '/inicio', component: () => import('pages/Home.vue') },
+      { path: '/contratos', component: () => import('pages/Contrato.vue') }
     ]
   },
 
   { path: '/login', component: () => import('pages/Login.vue') },
-  { path: '/contratos', component: () => import('pages/Contrato.vue') },
 
   {
     path: '/',
